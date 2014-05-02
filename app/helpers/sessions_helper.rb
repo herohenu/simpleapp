57ea7cb9aae245350e02c9b19cf71f3fcba15725
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-02 11:43:02 +0800

    ****fix error of  signin  & current user method ***

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,14 +10,15 @@ module SessionsHelper
   end
 
   def current_user=(user)
-    def current_user
-      remember_token = User.hash(cookies[:remember_token])
-      @current_user ||= User.find_by(remember_token: remember_token)
-    end
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.hash(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 
   def signed_in?
     !current_user.nil?
   end
-  
 end

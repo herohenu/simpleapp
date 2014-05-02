9deadcefbe676fd936fc1ef9e838300867128208
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-02 08:57:06 +0800

    ****generate  seesion controller and pages***

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "登录成功!"
+      sign_in user
+      redirect_to user
+    else
+      # Create an error message and re-render the signin form.
+      flash[:error] = "用户名或密码错误!"
+      render 'new'
+    end
+  end
+end

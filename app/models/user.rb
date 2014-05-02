848c346440621affd256abe62afce8e358365496
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-02 11:18:30 +0800

    ****def signin  & current user method ***

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,28 @@
 class User < ActiveRecord::Base
+  #email 不区分大小写
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   validates :name,  presence: true, length: { maximum: 10 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #唯一性验证无法真正保证唯一性。
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  #email 不区分大小写
-  before_save { self.email = email.downcase }
-
   validates :password, length: { minimum: 6 }
+
   has_secure_password
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      # Create the token.
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end

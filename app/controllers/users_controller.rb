036ff0d2cebc1a57d6f7c69d841518b03d2686d1
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-04 23:43:43 +0800

    ****store path if visit resource should login  ***

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,11 @@ class UsersController < ApplicationController
   end
 
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    #redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless  signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   #user only can edit self profile

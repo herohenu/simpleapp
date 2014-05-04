d469a0ca22e9a1155bc0f3796878357e309373c8
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-04 23:27:58 +0800

    ****check currentuser & remember path befor login  ***

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,10 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -27,6 +31,17 @@ module SessionsHelper
                                   User.hash(User.new_remember_token))
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  #remember  the path before signin
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    #delete return_to url after the correct jump
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath if request.get?
   end
 
 end

573a495ab89777680e7056b43ffb3fbbba26c243
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-06 21:13:11 +0800

    ****Add posts model***

---
+++ db/migrate/20140506125134_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.string :content  , null:false
+      t.integer :user_id , null:false
+
+      t.timestamps
+    end
+    add_index :microposts, [:user_id, :created_at]
+  end
+end

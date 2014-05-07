9be2b69d7299f7d6d97a79e7a7865dba292715d4
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-05-08 00:01:34 +0800

    ****feed list to show microposts & refacting ***

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ class User < ActiveRecord::Base
 
   def feed
     # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    #Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   #判断是否已经关注了 other_user
